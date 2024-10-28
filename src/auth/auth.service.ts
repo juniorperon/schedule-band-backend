4eaa24aaa139d3c23f71a79624c565109c2553b9
ailtonperon <ailtonperon@valeti.com>
2024-10-28 11:35:42 -0300

    feature login with errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+  Logger,
+} from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -13,25 +19,38 @@ export class AuthService {
     private userRepository: Repository<User>,
     private jwtService: JwtService,
   ) {}
-
+  private readonly logger = new Logger(AuthService.name);
   async login(loginDto: LoginDto): Promise<{ token: string }> {
-    const user = await this.userRepository.findOne({
-      where: { email: loginDto.email },
-    });
+    try {
+      const user = await this.userRepository.findOne({
+        where: { email: loginDto.email },
+      });
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
 
-    if (!user) {
-      throw new Error('Usuário não encontrado');
+      const isPasswordValid = await bcrypt.compare(
+        loginDto.password,
+        user.password,
+      );
+      if (!isPasswordValid) {
+        throw new UnauthorizedException('Senha inválida');
+      }
+
+      const token = this.jwtService.sign({ id: user.id });
+      return { token };
+    } catch (error) {
+      this.logger.error(
+        `Erro ao realizar login: ${error.message}`,
+        error.stack,
+      );
+      if (
+        error instanceof NotFoundException ||
+        error instanceof UnauthorizedException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Erro ao realizar login'); // Para erros desconhecidos
     }
-
-    const isPasswordValid = await bcrypt.compare(
-      loginDto.password,
-      user.password,
-    );
-    if (!isPasswordValid) {
-      throw new Error('Senha inválida');
-    }
-
-    const token = this.jwtService.sign({ id: user.id });
-    return { token };
   }
 }
